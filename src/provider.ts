2c3de077af900079cab30ecdd70e17b87700bda3
muchlishchoeruddin <muchlishchoeruddin96@gmail.com>
2023-05-29 09:07:48 +0700

    Fix controller implementation

--- src/provider.ts
+++ src/provider.ts
@@ -1,6 +1,7 @@
 import EnvConfiguration from './config';
 import {Logger} from 'winston';
 import Controller from './server/controllers';
+import Service from './server/services';
 
 interface ProviderConstructor {
     config: EnvConfiguration
@@ -10,7 +11,8 @@ interface ProviderConstructor {
 export default class Provider {
     public config: EnvConfiguration
     public logger: Logger
-    public controller?: Controller
+    public controller!: Controller
+    public service!: Service
     constructor({ config, logger }: ProviderConstructor) {
         this.config = config
         this.logger = logger
--- src/server/base/base-controller.ts
+++ src/server/base/base-controller.ts
@@ -1,8 +1,12 @@
 import Provider from '../../provider';
+import {Logger} from 'winston';
 
-export default abstract class BaseController {
+export default class BaseController {
     provider!: Provider
-    init(provider: Provider) {
+    logger!: Logger
+
+    init(provider: Provider, logger: Logger) {
         this.provider = provider
+        this.logger = logger
     }
 }
