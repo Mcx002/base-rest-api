29edb7eea78c774e2ec85fcd31bf8019c9b1676a
muchlishchoeruddin <muchlishchoeruddin96@gmail.com>
2023-05-30 07:51:36 +0700

    Add user service

--- src/provider.ts
+++ src/provider.ts
@@ -2,18 +2,16 @@ import EnvConfiguration from './config';
 import {Logger} from 'winston';
 import Controller from './server/controllers';
 import Service from './server/services';
-
-interface ProviderConstructor {
-    config: EnvConfiguration
-    logger: Logger
-}
+import Repository from './server/repositories';
 
 export default class Provider {
     public config: EnvConfiguration
     public logger: Logger
     public controller!: Controller
     public service!: Service
-    constructor({ config, logger }: ProviderConstructor) {
+    public repository!: Repository
+
+    constructor(config: EnvConfiguration, logger: Logger) {
         this.config = config
         this.logger = logger
     }
--- src/server/services/index.ts
+++ src/server/services/index.ts
@@ -2,17 +2,21 @@ import Provider from '../../provider';
 import BaseService from '../base/base-service';
 import {HealthService} from './health.service';
 import {Logger} from 'winston';
+import {UserService} from './user.service';
 
 export default class Service {
     private readonly provider: Provider
     private readonly logger: Logger
 
+    // Service Store
     public healthService: HealthService = new HealthService()
+    public userService: UserService = new UserService()
 
     constructor(provider: Provider) {
         this.provider = provider
         this.logger = provider.logger.child({childLabel: 'Service'})
 
+        // Initiate Service
         for (const item in this) {
             if (!(this[item] instanceof BaseService)) {
                 continue
