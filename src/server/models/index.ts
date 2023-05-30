e94fca73011f1422ea4e6a2e340975db2a4ddb27
muchlishchoeruddin <muchlishchoeruddin96@gmail.com>
2023-05-30 07:49:22 +0700

    Add user model

---
+++ src/server/models/index.ts
@@ -0,0 +1,15 @@
+import {User} from './user.model';
+import Provider from '../../provider';
+import DbContext from '../../db-context';
+
+export default class Model {
+    public dbContext: DbContext
+    constructor(provider: Provider) {
+        this.dbContext = new DbContext(provider)
+        const sequelize = this.dbContext.sequelize
+
+        // Initiate Model
+        User.initModel(sequelize)
+    }
+}
+
---
+++ src/server/models/user.model.ts
@@ -0,0 +1,44 @@
+import { DataTypes, Model, Optional, Sequelize} from 'sequelize';
+
+export interface UserAttributes {
+    id: number
+    xid: string
+    name: string
+    email: string
+}
+
+export type UserCreationAttributes = Optional<UserAttributes, 'id'>
+
+export class User extends Model {
+    id!: number
+    xid!: string
+    name!: string
+    email!: string
+
+    static initModel(sequelize: Sequelize) {
+        User.init({
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            xid: {
+                type: DataTypes.STRING,
+                unique: true,
+                allowNull: false,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false
+            }
+        }, {
+            sequelize,
+            tableName: 'User',
+            timestamps: false
+        })
+    }
+}
