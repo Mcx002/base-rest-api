6b97ade5437cd7a4be08d01afa6fda0a98b03647
muchlishchoeruddin <muchlishchoeruddin96@gmail.com>
2023-05-30 07:48:26 +0700

    Add db context

---
+++ src/db-context.ts
@@ -0,0 +1,36 @@
+import {NodeEnvType} from './config';
+import {Dialect, Sequelize} from 'sequelize';
+import {Logger} from 'winston';
+import Provider from './provider';
+
+export default class DbContext {
+    private logger: Logger
+    sequelize: Sequelize
+
+    constructor(provider: Provider) {
+        this.logger = provider.logger.child({childLabel: 'DbContext'})
+        const config = provider.config
+
+        this.sequelize = new Sequelize(
+            config.dbName,
+            config.dbUsername,
+            config.dbPassword,
+            {
+                dialect: config.dbDialect as Dialect,
+                host: config.dbHost,
+                port: config.dbPort,
+                logging: config.nodeEnv !== NodeEnvType.Test
+            }
+        )
+    }
+
+    checkConnection = async (): Promise<boolean> => {
+        try {
+            await this.sequelize.authenticate()
+            this.logger.info('Database Connected')
+            return true
+        } catch (e) {
+            throw new Error('Unable to connect to the database')
+        }
+    }
+}
