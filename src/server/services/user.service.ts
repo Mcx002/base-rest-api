98f711458b98a1498b61988e867ea08f1fa02e30
muchlishchoeruddin <muchlishchoeruddin96@gmail.com>
2023-06-22 23:08:12 +0700

    Add create user on service

--- src/server/services/user.service.ts
+++ src/server/services/user.service.ts
@@ -1,11 +1,13 @@
 import BaseService from '../base/base-service'
 import { GetDetailPayload } from '../../dto/common.dto'
 import { UserDto } from '../../dto/user.dto'
-import { UserAttributes } from '../models/user.model'
+import { UserAttributes, UserCreationAttributes } from '../models/user.model'
 import { Logger } from 'winston'
 import Provider from '../../provider'
 import { UserRepository } from '../repositories/user.repository'
-import { errors } from '../constants/error.constant'
+import { errors, handleError } from '../constants/error.constant'
+import * as randomstring from 'randomstring'
+import { ServerError } from '../../utils/errors'
 
 export class UserService extends BaseService {
     log!: Logger
@@ -34,5 +36,29 @@ export class UserService extends BaseService {
             name: row.name,
             email: row.email,
         }
+    }
+
+    createUser = async (payload: UserDto): Promise<UserDto> => {
+        // Prepare insert value
+        const userForm: UserCreationAttributes = {
+            xid: randomstring.generate(6),
+            name: payload.name,
+            email: payload.email,
+        }
+
+        // Persist insert data
+        let data: UserAttributes
+        try {
+            data = await this.userRepository.insert(userForm)
+        } catch (e) {
+            // error handlers
+            handleError.uniqueConstraint(e, 'Failed to insert user data, please try again')
+
+            // default error
+            throw new ServerError('Failed to insert user data')
+        }
+
+        // compose and return
+        return this.composeUser(data)
     }
 }
